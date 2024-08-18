88ae8d88ac28557e2989c742aadb7c6024347b3a
Gustavo Botelho Vieira <gu-botelho@hotmail.com>
2024-08-18 18:06:07 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Jornada de Dados")
+
+
+# Variáveis
+
+idade = 29
+Nome = "Gustavo"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade +1
+print(nova_idade)
+
+# lista
+
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
+print(lista)
+print(lista_invertida)
